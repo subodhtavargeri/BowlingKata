5d3f8992401ffb03349652a08ff5209fa25b6b73
Subodh Tavargeri <subodhtavargeri@Subodhs-MacBook-Air.local>
2022-11-02 15:24:07 +0530

    add test_GameScoreIsSixteen_WhenPlayerKnockFiveThenFiveThenThree_InAllTwentOneRolls_SpareCondition and logic to check spare

--- Bowling/Game/Game.swift
+++ Bowling/Game/Game.swift
@@ -2,12 +2,28 @@ class Game {
     
     private (set) var score: Int
     
+    private var rolls = [Int]()
+    
     init() {
         score = 0
     }
     
     func rollBalls(pins: Int) {
-        score += pins
+        rolls.append(pins)
+        
     }
     
+    func getscore() {
+        var rollIndex = 0
+        for _ in 1...10 {
+            let frameScore = rolls[rollIndex] + rolls[rollIndex+1]
+            if frameScore == 10 {
+                score += frameScore + rolls[rollIndex+2]
+            }
+            else {
+                score += frameScore
+            }
+            rollIndex+=2
+        }
+    }
 }
--- BowlingTests/GameTests.swift
+++ BowlingTests/GameTests.swift
@@ -14,7 +14,8 @@ class GameTests: XCTestCase {
     }
     
     private func rollBalls(knockedPins: Int, roll: Int) {
-        for _ in 1...roll {
+        for index in 1...roll {
+            print("index::\(index)")
             let _ = game?.rollBalls(pins: knockedPins)
         }
     }
@@ -32,11 +33,23 @@ class GameTests: XCTestCase {
     }
     
     func test_GameScoreIsTwelve_WhenPlayerKnocksFourThenOneThenFiveThenTwoPins_InFirstFourRolls() {
+        rollBalls(knockedPins: 0, roll: 16)
         game?.rollBalls(pins: 4)
         game?.rollBalls(pins: 1)
         game?.rollBalls(pins: 5)
         game?.rollBalls(pins: 2)
         
         XCTAssertEqual(game?.score, 12)
+    }
+    
+    func test_GameScoreIsSixteen_WhenPlayerKnockFiveThenFiveThenThree_InAllTwentOneRolls_SpareCondition() {
+        
+        game?.rollBalls(pins: 5)
+        game?.rollBalls(pins: 5)
+        game?.rollBalls(pins: 3)
+        rollBalls(knockedPins: 0, roll: 18)
+        
+        game?.getscore()
+        XCTAssertEqual(game?.score, 16)
     }
 }
