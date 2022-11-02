c12751080efee98bff11f73615f599fd40450534
Subodh Tavargeri <subodhtavargeri@Subodhs-MacBook-Air.local>
2022-11-02 18:10:01 +0530

    extract normalScore function

--- Bowling/Game/Game.swift
+++ Bowling/Game/Game.swift
@@ -16,36 +16,39 @@ class Game {
         var rollIndex = 0
         for _ in 1...10 {
             
-            if isStrike(score: rolls[rollIndex]) {
-                strikeBonusScore(rollIndex: rollIndex)
+            let rollScore = rolls[rollIndex]
+            if isStrike(score: rollScore) {
+                score += strikeBonusScore(rollIndex: rollIndex)
                 rollIndex+=1
                 continue
             }
             
             let frameScore = rolls[rollIndex] + rolls[rollIndex+1]
-            
             if isSpare(frameScore: frameScore) {
-                spareBonusScore(rollIndex: rollIndex)
+                score += spareBonusScore(rollIndex: rollIndex)
             }
             else {
-                score += frameScore
+                score += normalScore(rollIndex: rollIndex)
             }
             rollIndex+=2
         }
     }
     
+    private func normalScore(rollIndex: Int)-> Int {
+        return rolls[rollIndex] + rolls[rollIndex+1]
+    }
     private func isSpare(frameScore: Int)-> Bool {
         return frameScore == 10
     }
     
-    private func spareBonusScore(rollIndex: Int) {
-        score += rolls[rollIndex] + rolls[rollIndex+1] + rolls[rollIndex+2]
+    private func spareBonusScore(rollIndex: Int)-> Int {
+        return rolls[rollIndex] + rolls[rollIndex+1] + rolls[rollIndex+2]
     }
     
     private func isStrike(score: Int)-> Bool {
         return score == 10
     }
-    private func strikeBonusScore(rollIndex: Int) {
-        score += rolls[rollIndex]  + rolls[rollIndex + 1]  + rolls[rollIndex + 2]
+    private func strikeBonusScore(rollIndex: Int)-> Int {
+        return rolls[rollIndex]  + rolls[rollIndex + 1]  + rolls[rollIndex + 2]
     }
 }
