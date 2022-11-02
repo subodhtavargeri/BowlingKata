09cb286764fd570b81777aefd95e13213eb74920
Subodh Tavargeri <subodhtavargeri@Subodhs-MacBook-Air.local>
2022-11-02 12:35:51 +0530

    refactor GameTests

--- BowlingTests/GameTests.swift
+++ BowlingTests/GameTests.swift
@@ -3,15 +3,13 @@ import XCTest
 
 class GameTests: XCTestCase {
     
+    let game = Game()
+    
     func test_GameScoreIsZero_WhenGameBegins() {
-        let game = Game()
-        
         XCTAssertEqual(game.score, 0)
     }
     
     func test_GameScoreIsZero_WhenPlayerDoesNotKnockAnyPins_InAllTwentyFrames() {
-        let game = Game()
-        
         for _ in 1...20 {
             let _ = game.roll(pins: 0)
         }
