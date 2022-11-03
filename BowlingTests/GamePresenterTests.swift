e809cafa61ae805b123b2d06a34d6ca75f61f95c
Subodh Tavargeri <subodhtavargeri@Subodhs-MacBook-Air.local>
2022-11-03 13:31:45 +0530

    add test_PlayerRollsScoreIsZero_WhenPlayerKnocksZeroPin

--- BowlingTests/GamePresenterTests.swift
+++ BowlingTests/GamePresenterTests.swift
@@ -12,4 +12,13 @@ class GamePresenterTests: XCTestCase {
         XCTAssertEqual(game.pins, Pin.two)
     }
     
+    func test_PlayerRollsScoreIsZero_WhenPlayerKnocksZeroPin() {
+        let game = GameSpy()
+        let presenter = GamePresenter(game: game)
+        
+        presenter.playerRolls(knockedPins: 0)
+        
+        XCTAssertEqual(game.pins, Pin.zero)
+    }
+    
 }
