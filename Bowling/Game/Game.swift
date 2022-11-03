4d6428471f4e07e3783d11a5eef651eae51a69e3
Subodh Tavargeri <subodhtavargeri@Subodhs-MacBook-Air.local>
2022-11-03 12:30:45 +0530

    refactor Game file for better readability

--- Bowling/Game/Game.swift
+++ Bowling/Game/Game.swift
@@ -5,7 +5,7 @@ enum Pin: Int {
 class Game {
     
     private (set) var score: Int
-     
+    
     private var rolls = [Pin]()
     
     init() {
@@ -19,8 +19,8 @@ class Game {
     func getscore() {
         var rollIndex = 0
         for _ in 1...10 {
-            
             let rollScore = rolls[rollIndex]
+            
             if isStrike(score: rollScore) {
                 score += strikeBonusScore(rollIndex: rollIndex)
                 rollIndex+=1
@@ -29,20 +29,22 @@ class Game {
             
             if isSpare(rollIndex: rollIndex) {
                 score += spareBonusScore(rollIndex: rollIndex)
+                rollIndex+=2
+                continue
             }
             else {
                 score += normalScore(rollIndex: rollIndex)
+                rollIndex+=2
             }
-            rollIndex+=2
         }
     }
     
     private func normalScore(rollIndex: Int)-> Int {
         return rolls[rollIndex].rawValue + rolls[rollIndex+1].rawValue
     }
+    
     private func isSpare(rollIndex: Int)-> Bool {
-        let frameScore = rolls[rollIndex].rawValue + rolls[rollIndex+1].rawValue
-        return frameScore == 10
+        return rolls[rollIndex].rawValue + rolls[rollIndex+1].rawValue == 10
     }
     
     private func spareBonusScore(rollIndex: Int)-> Int {
@@ -52,6 +54,7 @@ class Game {
     private func isStrike(score: Pin)-> Bool {
         return score.rawValue == 10
     }
+    
     private func strikeBonusScore(rollIndex: Int)-> Int {
         return rolls[rollIndex].rawValue  + rolls[rollIndex + 1].rawValue  + rolls[rollIndex + 2].rawValue
     }
