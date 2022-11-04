c3823cb06f71182222f29c3d6d4961c89ebb6cff
Subodh Tavargeri <subodhtavargeri@Subodhs-MacBook-Air.local>
2022-11-04 13:10:28 +0530

    update BowlingViewController.swift

--- Bowling/Game/BowlingViewController.swift
+++ Bowling/Game/BowlingViewController.swift
@@ -1,5 +1,14 @@
+import UIKit
+
 protocol BowlingViewProtocol {
     func displayPinRollTitle(title: String)
     func displayFinalGameScore(score: String)
     func displayViewTitle(title: String)
+}
+
+class BowlingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
